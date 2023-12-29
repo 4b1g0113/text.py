00668adf28707fa895119a5805b2fb5730070a2a
4b1g0113 <128337667+4b1g0113@users.noreply.github.com>
2023-12-29 10:25:51 +0800

    12/29第一次修改
    12/292

---
+++ text.py
@@ -0,0 +1,18 @@
+class LibBook:
+    def__init__(self,book_id):
+        self.book_id=book_id
+
+
+
+
+class Fruit:
+    def__init__(self):
+
+class Car:
+    def__init__(self):
+
+class School:
+    def__init__(self):
+
+class Sports:
+    def__init__(self):
